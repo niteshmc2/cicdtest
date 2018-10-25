50a5ddb15fa3134e465cdc234eb4d9465e1f2eba
Nitesh <machireddychaitany.n@husky.neu.edu>
2018-10-25 05:37:53 -0400

    spec

--- Infrastructure/aws/codedeploy/applicationStart.sh
+++ Infrastructure/aws/codedeploy/applicationStart.sh
@@ -2,6 +2,8 @@
 
 pwd
 
+cd /centos/home
+
 export RDS_HOSTNAME=`sed -n '1p' .env`
 export RDS_USERNAME=`sed -n '2p' .env`
 export RDS_PASSWORD=`sed -n '3p' .env`
@@ -19,6 +21,8 @@ sudo npm install -g pm2
 echo "Check dir"
 
 pwd
+
+ls -a
 
 pwd 
 
